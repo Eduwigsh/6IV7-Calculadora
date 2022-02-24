a6c8f6df36da5a1698b190e5bb8234231537ddd7
Eduwigsh <esolisa1900@alumno.ipn.mx>
2022-02-24 00:03:19 -0600

    parte final

--- app/src/main/java/com/example/calculadora/MainActivity2.java
+++ app/src/main/java/com/example/calculadora/MainActivity2.java
@@ -17,7 +17,7 @@ String promat1,promat2,promat3,promat4,promat5,promat6,promat7,promat8,promat9;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         columna1= findViewById(R.id.Extras);
-        columna1= findViewById(R.id.masamenos);
+        columna2= findViewById(R.id.masamenos);
          promat1= getIntent().getStringExtra("promat1");
          promat2= getIntent().getStringExtra("promat2");
          promat3=getIntent().getStringExtra("promat3");
@@ -29,6 +29,7 @@ String promat1,promat2,promat3,promat4,promat5,promat6,promat7,promat8,promat9;
          promat9= getIntent().getStringExtra("promat9");
     extras();
     lista();
+
     }
     public void lista (){
         Double m1= Double.parseDouble(promat1);
@@ -65,11 +66,12 @@ String promat1,promat2,promat3,promat4,promat5,promat6,promat7,promat8,promat9;
                 }
             }
         }
-        TextView M= new TextView(MainActivity2.this);
+
         for(int i = 0; i < 9; i++){
             if(a[i]==m1){
 
                 if(M1 != 1 ) {
+                    TextView M= new TextView(MainActivity2.this);
                     M.setText("materia 1");
                     columna2.addView(M);
                     M1=1;
@@ -78,6 +80,7 @@ String promat1,promat2,promat3,promat4,promat5,promat6,promat7,promat8,promat9;
             if(a[i]==m2){
 
                 if(M2 != 1 ) {
+                    TextView M= new TextView(MainActivity2.this);
                     M.setText("materia 2");
                     columna2.addView(M);
                     M2=1;
@@ -86,6 +89,7 @@ String promat1,promat2,promat3,promat4,promat5,promat6,promat7,promat8,promat9;
             if(a[i]==m3){
 
                 if(M3 != 1 ) {
+                    TextView M= new TextView(MainActivity2.this);
                     M.setText("materia 3");
                     columna2.addView(M);
                     M3=1;
@@ -94,6 +98,7 @@ String promat1,promat2,promat3,promat4,promat5,promat6,promat7,promat8,promat9;
             if(a[i]==m4){
 
                 if(M4 != 1 ) {
+                    TextView M= new TextView(MainActivity2.this);
                     M.setText("materia 4");
                     columna2.addView(M);
                     M4=1;
@@ -102,6 +107,7 @@ String promat1,promat2,promat3,promat4,promat5,promat6,promat7,promat8,promat9;
             if(a[i]==m5){
 
                 if(M5 != 1 ) {
+                    TextView M= new TextView(MainActivity2.this);
                     M.setText("materia 5");
                     columna2.addView(M);
                     M5=1;
@@ -110,6 +116,7 @@ String promat1,promat2,promat3,promat4,promat5,promat6,promat7,promat8,promat9;
             if(a[i]==m6){
 
                 if(M6 != 1 ) {
+                    TextView M= new TextView(MainActivity2.this);
                     M.setText("materia 6");
                     columna2.addView(M);
                     M6=1;
@@ -118,6 +125,7 @@ String promat1,promat2,promat3,promat4,promat5,promat6,promat7,promat8,promat9;
             if(a[i]==m7){
 
                 if(M7 != 1 ) {
+                    TextView M= new TextView(MainActivity2.this);
                     M.setText("materia 7");
                     columna2.addView(M);
                     M7=1;
@@ -126,6 +134,7 @@ String promat1,promat2,promat3,promat4,promat5,promat6,promat7,promat8,promat9;
             if(a[i]==m8){
 
                 if(M8 != 1 ) {
+                    TextView M= new TextView(MainActivity2.this);
                     M.setText("materia 8");
                     columna2.addView(M);
                     M8=1;
@@ -134,6 +143,7 @@ String promat1,promat2,promat3,promat4,promat5,promat6,promat7,promat8,promat9;
             if(a[i]==m9){
 
                 if(M9 != 1 ) {
+                    TextView M= new TextView(MainActivity2.this);
                     M.setText("materia 9");
                     columna2.addView(M);
                     M9=1;
@@ -143,6 +153,7 @@ String promat1,promat2,promat3,promat4,promat5,promat6,promat7,promat8,promat9;
         }
     }
     public void extras(){
+
         Double m1= Double.parseDouble(promat1);
         Double m2= Double.parseDouble(promat2);
         Double m3= Double.parseDouble(promat3);
@@ -152,50 +163,50 @@ String promat1,promat2,promat3,promat4,promat5,promat6,promat7,promat8,promat9;
         Double m7= Double.parseDouble(promat7);
         Double m8= Double.parseDouble(promat8);
         Double m9= Double.parseDouble(promat9);
-        TextView M= new TextView(MainActivity2.this);
-        if(m1<6){
 
+        if(m1<6){
+            TextView M= new TextView(MainActivity2.this);
             M.setText("materia 1");
             columna1.addView(M);
         }
         if(m2<6){
-
-            M.setText("materia 1");
+            TextView M= new TextView(MainActivity2.this);
+            M.setText("materia 2");
             columna1.addView(M);
         }
         if(m3<6){
-
-            M.setText("materia 1");
+            TextView M= new TextView(MainActivity2.this);
+            M.setText("materia 3");
             columna1.addView(M);
         }
         if(m4<6){
-
-            M.setText("materia 1");
+            TextView M= new TextView(MainActivity2.this);
+            M.setText("materia 4");
             columna1.addView(M);
         }
         if(m5<6){
-
-            M.setText("materia 1");
+            TextView M= new TextView(MainActivity2.this);
+            M.setText("materia 5");
             columna1.addView(M);
         }
         if(m6<6){
-
-            M.setText("materia 1");
+            TextView M= new TextView(MainActivity2.this);
+            M.setText("materia 6");
             columna1.addView(M);
         }
         if(m7<6){
-
-            M.setText("materia 1");
+            TextView M= new TextView(MainActivity2.this);
+            M.setText("materia 7");
             columna1.addView(M);
         }
         if(m8<6){
-
-            M.setText("materia 1");
+            TextView M= new TextView(MainActivity2.this);
+            M.setText("materia 8");
             columna1.addView(M);
         }
         if(m9<6){
-
-            M.setText("materia 1");
+            TextView M= new TextView(MainActivity2.this);
+            M.setText("materia 9");
             columna1.addView(M);
         }
 
